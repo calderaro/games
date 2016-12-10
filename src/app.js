21889ddbefe1aa0864b2b3ad4ec57863da23bcd2
Angel Calderaro <calderaro94@gmail.com>
2016-12-09 23:30:08 -0400

    tiles

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ import window from 'global/window'
 import sio from 'socket.io-client'
 import keyboard from 'keyboardjs'
 import Player from './player'
+import map from './map'
 
 const dead = new Image()
+const floors = new Image()
 dead.src = 'static/img/sprites/11.gif'
+floors.src = 'static/img/sprites/73145.png'
 
 let state = { vx: 0, vy: 0, players: {} }
 let keys = []
@@ -59,12 +62,61 @@ function draw (time) {
   const players = Object.values(state.players)
   const player = players.filter(p => p.name === name)[0] || {}
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  for (let y = 0; y <= 63; y++) {
+    for (let x = 0; x <= 63; x++) {
+      const dx = (map.x - player.x) + ctx.canvas.width / 2 + (x * 31)
+      const dy = (map.y - player.y) + ctx.canvas.height / 2 + (y * 31)
+      switch (map.tiles[x + 64 * y]) {
+        case 0:
+          ctx.drawImage(floors,
+            32 * 5,
+            32 * 11,
+            32, 32,
+            dx,
+            dy,
+            32, 32
+          )
+          break
+        case 1:
+          ctx.drawImage(floors,
+            32 * 6,
+            32 * 11,
+            32, 32,
+            dx,
+            dy,
+            32, 32
+          )
+          break
+        case 2:
+          ctx.drawImage(floors,
+            32 * 7,
+            32 * 11,
+            32, 32,
+            dx,
+            dy,
+            32, 32
+          )
+          break
+        default:
+          ctx.drawImage(floors,
+            32 * 6,
+            32 * 11,
+            32, 32,
+            dx,
+            dy,
+            32, 32
+          )
+      }
+    }
+  }
+
   ctx.fillStyle = 'red'
   players.filter(p => p.name !== name).forEach(p => {
     if (p.status === 'dead') {
-      ctx.drawImage(dead, 0, 0, 32, 32, p.x - player.x + canvas.width / 2, p.y - player.y + canvas.height / 2, 32, 32)
+      ctx.drawImage(dead, 0, 0, 32, 32, p.x - player.x + ctx.canvas.width / 2, p.y - player.y + ctx.canvas.height / 2, 32, 32)
     } else {
-      const other = {...p, x: p.x - player.x + canvas.width / 2, y: p.y - player.y + canvas.height / 2}
+      const other = {...p, x: p.x - player.x, y: p.y - player.y}
       Player(ctx, time, other, true)
     }
   })
--- src/player.js
+++ src/player.js
@@ -1,21 +1,21 @@
 const character = new Image()
 character.src = 'static/img/sprites/44808.png'
 
-export default function (ctx, delta, player, other=false) {
-  const animationSpeed = parseInt(delta / 250)
+export default function (ctx, delta, player, other = false) {
+  const animationSpeed = parseInt(delta / 250, 10)
   let animation = 'stand'
 
   const animations = {
     stand: [
-      { x: 5, y: 11 },
+      { x: 5, y: 11 }
     ],
     left: [
-      { x: 11, y: 9},
-      { x: 7, y: 11}
+      { x: 11, y: 9 },
+      { x: 7, y: 11 }
     ],
     right: [
-      { x: 3, y: 11},
-      { x: 0, y: 8}
+      { x: 3, y: 11 },
+      { x: 0, y: 8 }
     ],
     down: [
       { x: 5, y: 11 },
@@ -31,10 +31,9 @@ export default function (ctx, delta, player, other=false) {
   if (player.vx === 1) animation = 'right'
   if (player.vy === -1) animation = 'up'
   if (player.vy === 1) animation = 'down'
-
   ctx.drawImage(character,
-    0 + 64 * animations[animation][animationSpeed % animations[animation].length].x,
-    0 + 64 * animations[animation][animationSpeed % animations[animation].length].y,
+    64 * animations[animation][animationSpeed % animations[animation].length].x,
+    64 * animations[animation][animationSpeed % animations[animation].length].y,
     66, 66,
     (other ? player.x : ctx.canvas.width / 2) - 32,
     (other ? player.y : ctx.canvas.height / 2) - 32,
